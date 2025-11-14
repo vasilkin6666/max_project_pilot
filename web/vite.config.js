7756d2b92b6ba74aad75e9108b19aaa47cd0f9f7
Code For Everyone <vasilkin666@gmail.com>
2025-11-14 15:11:46 +0300

    1

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,27 +1,22 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig({
-    plugins: [],
-    base: '/max_project_pilot/',
-    build: {
-        outDir: 'dist',
-        sourcemap: false,
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'index.html')
-            }
-        },
-        // Добавьте эту настройку для копирования статических файлов
-        assetsDir: 'assets',
-        copyPublicDir: true
-    },
-    publicDir: 'public',
-    server: {
-        port: 3000,
-        open: true
-    },
-    optimizeDeps: {
-        include: ['react', 'react-dom']
+  plugins: [],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
     }
+  },
+  server: {
+    port: 3000,
+    open: true
+  },
+  optimizeDeps: {
+    include: ['react', 'react-dom']
+  }
 })
