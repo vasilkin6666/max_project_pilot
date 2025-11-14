30e3b8478485155b9ddc248613222ee9e98f39af
Code For Everyone <vasilkin666@gmail.com>
2025-11-14 15:02:10 +0300

    Update vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
     plugins: [],
@@ -8,10 +9,14 @@ export default defineConfig({
         sourcemap: false,
         rollupOptions: {
             input: {
-                main: './index.html'
+                main: resolve(__dirname, 'index.html')
             }
-        }
+        },
+        // Добавьте эту настройку для копирования статических файлов
+        assetsDir: 'assets',
+        copyPublicDir: true
     },
+    publicDir: 'public',
     server: {
         port: 3000,
         open: true
